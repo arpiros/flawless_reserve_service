34a5b44b8853f9683627154ddd85e2ce31ee1ec4
arpiros <arpiros@naver.com>
2020-09-12 17:33:38 +0900

    set echo framework

--- src/flawless_reserve_service/main.go
+++ src/flawless_reserve_service/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
 func main() {
 	println("Start flawless_reserve_service")
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
